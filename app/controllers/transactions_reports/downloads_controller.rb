6391ee43a11732221af9f78113adcf07b593e0f9
Keymar <cloraltkeymar22@gmail.com>
2026-02-05 22:22:35 -0400

    Add manual reports

---
+++ app/controllers/transactions_reports/downloads_controller.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module TransactionsReports
+  class DownloadsController < ApplicationController
+    before_action :authenticate_user!
+
+    def show
+      @report = current_user.account.transactions_reports.find(params[:transactions_report_id])
+
+      unless @report.file_attached?
+        redirect_to transactions_report_path(@report),
+                    alert: "El archivo del reporte no está disponible"
+        return
+      end
+
+      send_data @report.file.download,
+                filename: @report.file.filename.to_s,
+                type: @report.file.content_type,
+                disposition: "attachment"
+    rescue ActiveRecord::RecordNotFound
+      redirect_to transactions_reports_path, alert: "Reporte no encontrado"
+    end
+  end
+end
