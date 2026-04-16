ba20bb1c3d9905cdf7610d47739fb01d387a99c1
Keymar <cloraltkeymar22@gmail.com>
2026-04-15 23:50:19 -0400

    Refactor pagination component; simplify ERB template and update CSS for navigation alignment

--- app/components/pagination_component.rb
+++ app/components/pagination_component.rb
@@ -2,9 +2,7 @@
 
 class PaginationComponent < ApplicationComponent
   erb_template <<~ERB
-    <%= content_tag :div, class: 'w-100 flex justify-center items-center gap-2' do %>
-      <%== @pagy.series_nav %>
-    <% end %>
+    <%== @pagy.series_nav %>
   ERB
 
   def initialize(pagy:)
